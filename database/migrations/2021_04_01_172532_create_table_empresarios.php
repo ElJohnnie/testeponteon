226542ef430d1252bb48efcac8cd64c726f21977
ElJohnnie <johnfollmer1995@gmail.com>
2021-04-01 15:58:00 -0300

    make first migration

---
+++ database/migrations/2021_04_01_172532_create_table_empresarios.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableEmpresarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('celular');
+            $table->string('estado');
+            $table->string('cidade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresarios');
+    }
+}
