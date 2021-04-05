1c2eb1e568224b7fd2b9c984a693141bc7042624
ElJohnnie <johnfollmer1995@gmail.com>
2021-04-05 13:46:32 -0300

    database

--- testeponteon.sql
+++ testeponteon.sql
@@ -1,3 +1,11 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 05-Abr-2021 às 16:44
+-- Versão do servidor: 5.7.26
+-- versão do PHP: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -5,6 +13,12 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
 -- Database: `testeponteon`
 --
 
@@ -26,24 +40,7 @@ CREATE TABLE IF NOT EXISTS `empresarios` (
   `pai_id` bigint(20) UNSIGNED DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `empresarios_pai_id_foreign` (`pai_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Extraindo dados da tabela `empresarios`
---
-
-INSERT INTO `empresarios` (`id`, `nome`, `celular`, `estado`, `cidade`, `created_at`, `updated_at`, `pai_id`) VALUES
-(1, '1', '(51) 98353-0072', 'RS', 'Paverama', '2021-04-05 16:12:39', '2021-04-05 16:12:39', NULL),
-(2, '2', '(51) 98353-0071', 'PR', 'Apucarana', '2021-04-05 16:12:48', '2021-04-05 16:12:48', 1),
-(3, '3', '(33) 33333-3333', 'RJ', 'Angra dos Reis', '2021-04-05 16:21:06', '2021-04-05 16:21:06', 1),
-(4, '4', '(44) 54545-4545', 'PE', 'Abreu e Lima', '2021-04-05 16:22:51', '2021-04-05 16:22:51', 2),
-(5, 'Jonatha Follmer', '(12) 31231-2312', 'PR', 'Abatiá', '2021-04-05 16:25:54', '2021-04-05 16:25:54', 2),
-(6, '6', '(6', 'AM', 'Alvarães', '2021-04-05 16:26:07', '2021-04-05 16:26:07', 5),
-(7, '7', '(78) 77878-7878', 'AC', 'Acrelândia', '2021-04-05 16:34:40', '2021-04-05 16:34:40', 1),
-(8, '8', '(8', 'PE', 'Belém de Maria', '2021-04-05 16:34:50', '2021-04-05 16:34:50', 1),
-(9, 'teste', '(14) 23123-1231', 'RJ', 'Angra dos Reis', '2021-04-05 16:35:13', '2021-04-05 16:35:13', 6),
-(10, 'teste2', '(51) 98353-2131', 'AL', 'Água Branca', '2021-04-05 16:35:28', '2021-04-05 16:35:28', 9),
-(11, 'Jonatha Follmer 5', '(11) 11111-1111', 'RS', 'Paverama', '2021-04-05 16:35:37', '2021-04-05 16:35:37', 10);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -101,19 +98,7 @@ CREATE TABLE IF NOT EXISTS `pais` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Extraindo dados da tabela `pais`
---
-
-INSERT INTO `pais` (`id`, `nome`, `created_at`, `updated_at`) VALUES
-(1, '1', '2021-04-05 16:12:48', '2021-04-05 16:12:48'),
-(2, '3', '2021-04-05 16:22:51', '2021-04-05 16:22:51'),
-(5, 'Jonatha Follmer', '2021-04-05 16:26:07', '2021-04-05 16:26:07'),
-(6, '8', '2021-04-05 16:35:13', '2021-04-05 16:35:13'),
-(9, 'teste', '2021-04-05 16:35:28', '2021-04-05 16:35:28'),
-(10, 'teste2', '2021-04-05 16:35:37', '2021-04-05 16:35:37');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -154,7 +139,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Panteon Teste', 'teste@ponteon.com', '2021-04-05 16:12:24', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'FoCLDkYXYUt0gb8RHgr7awnWv9WJFgX6QLU6W3uPonsQs58JSDJ2Wi3Wxrvh', NULL, NULL);
+(1, 'Panteon Teste', 'teste@ponteon.com', '2021-04-05 19:43:58', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '1234', NULL, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
